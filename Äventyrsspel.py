cd8923618bb47af823040657d6a181a16c30c516
Tim Berghman <tim.berghman@gmail.com>
2021-11-12 15:51:58 +0100

    We're Doomed

---
+++ Äventyrsspel.py
@@ -0,0 +1,20 @@
+"""we're doomed"""
+
+
+class Player:
+    def __init__(self):
+        pass
+
+
+class Items:
+    def __init__(self):
+        pass
+
+
+class Enemy:
+    def __init__(self) -> None:
+        pass
+
+
+while True:
+    pass
