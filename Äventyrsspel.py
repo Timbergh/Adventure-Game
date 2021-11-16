38f1cdb089a7d8019ba529c9af36f1ccc18aeedb
Timbergh <tim.berghman@gmail.com>
2021-11-16 16:58:01 +0100

    :D

--- Äventyrsspel.py
+++ Äventyrsspel.py
@@ -1,5 +1,6 @@
 import random as rand
 import os
+import time
 
 
 class Player:
@@ -28,9 +29,19 @@ class Enemy:
 
 def clearConsole():
     command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+    if os.name in ('nt', 'dos'):
         command = 'cls'
     os.system(command)
+
+
+def random_encounter(rand_index):
+    if rand_index == 1:
+        re = "found an item"
+    if rand_index == 2:
+        re = "encounter a monster"
+    if rand_index == 3:
+        re = "found out you are standing on a trap!"
+    return re
 
 
 def doors():
@@ -39,8 +50,7 @@ def doors():
     left_open = False
     middle_open = False
     right_open = False
-    while choose_door != "q":
-        print("""
+    print("""
             |You walk into an unkown place and see three doors|
                 _____________   _____________   _____________
                 |           |   |           |   |           |
@@ -50,45 +60,166 @@ def doors():
                 |           |   |           |   |           |
                 |    [L]    |   |    [M]    |   |    [R]    |
                 |___________|   |___________|   |___________|
-            """)
+                """)
+    while choose_door != "q":
         choose_door = input(
             "Which door would you like to enter? [L] [M] [R] or [Q] quit to menu -> ").casefold()
-        if choose_door == "l":
-            clearConsole()
+        if choose_door == "l":  # LEFT DOOR
             if left_open == True:
-                print("There is nothing left here")
+                clearConsole()
+                print("""
+                _____________   _____________   _____________
+                |  |        |   |           |   |           |
+                |  |        |   |           |   |           |
+                | .|        |   |           |   |           |
+                |  |        |   |         * |   |         * |
+                |  |        |   |           |   |           |
+                | /         |   |    [M]    |   |    [R]    |
+                |/__________|   |___________|   |___________|
+
+                There is nothing left in this door
+                """)
             if left_open == False:
                 rand_index = rand.choice(rand_door)
                 rand_door.remove(rand_index)
+                time.sleep(0.3)
+                clearConsole()
+                print(f"""
+                _____________   _____________   _____________
+                |      |    |   |           |   |           |
+                |      |    |   |           |   |           |
+                |    * |    |   |           |   |           |
+                |      |    |   |         * |   |         * |
+                |      |    |   |           |   |           |
+                |    /      |   |    [M]    |   |    [R]    |
+                |__/________|   |___________|   |___________|
+
+                You opend the left door and {random_encounter(rand_index)}
+                """)
+                time.sleep(0.3)
+                clearConsole()
+                print(f"""
+                _____________   _____________   _____________
+                |  |        |   |           |   |           |
+                |  |        |   |           |   |           |
+                | .|        |   |           |   |           |
+                |  |        |   |         * |   |         * |
+                |  |        |   |           |   |           |
+                | /         |   |    [M]    |   |    [R]    |
+                |/__________|   |___________|   |___________|
+
+                You opend the left door and {random_encounter(rand_index)}
+                """)
             left_open = True
-        elif choose_door == "m":
-            clearConsole()
+
+        elif choose_door == "m":  # MIDDLE DOOR
             if middle_open == True:
-                print("There is nothing left here")
+                clearConsole()
+                print("""
+                _____________   _____________   _____________
+                |           |   |  |        |   |           |
+                |           |   |  |        |   |           |
+                |           |   | .|        |   |           |
+                |         * |   |  |        |   |         * |
+                |           |   |  |        |   |           |
+                |    [L]    |   | /         |   |    [R]    |
+                |___________|   |/__________|   |___________|
+
+                There is nothing left in this door
+                """)
             if middle_open == False:
                 rand_index = rand.choice(rand_door)
                 rand_door.remove(rand_index)
+                time.sleep(0.3)
+                clearConsole()
+                print(f"""
+                _____________   _____________   _____________
+                |           |   |      |    |   |           |
+                |           |   |      |    |   |           |
+                |           |   |   *  |    |   |           |
+                |         * |   |      |    |   |         * |
+                |           |   |      |    |   |           |
+                |    [L]    |   |    /      |   |    [R]    |
+                |___________|   |__/________|   |___________|
+
+                You opend the middle door and {random_encounter(rand_index)}
+                """)
+                time.sleep(0.3)
+                clearConsole()
+                print(f"""
+                _____________   _____________   _____________
+                |           |   |  |        |   |           |
+                |           |   |  |        |   |           |
+                |           |   | .|        |   |           |
+                |         * |   |  |        |   |         * |
+                |           |   |  |        |   |           |
+                |    [L]    |   | /         |   |    [R]    |
+                |___________|   |/__________|   |___________|
+
+                You opend the middle door and {random_encounter(rand_index)}
+                """)
             middle_open = True
-        elif choose_door == "r":
-            clearConsole()
+        elif choose_door == "r":  # RIGHT DOOR
             if right_open == True:
-                print("There is nothing left here")
+                clearConsole()
+                print("""
+                _____________   _____________   _____________
+                |           |   |           |   |  |        |
+                |           |   |           |   |  |        |
+                |           |   |           |   | .|        |
+                |         * |   |         * |   |  |        |
+                |           |   |           |   |  |        |
+                |    [L]    |   |    [M]    |   | /         |
+                |___________|   |___________|   |/__________|
+
+                There is nothing left in this door
+                """)
             if right_open == False:
                 rand_index = rand.choice(rand_door)
                 rand_door.remove(rand_index)
+                time.sleep(0.3)
+                clearConsole()
+                print(f"""
+                _____________   _____________   _____________
+                |           |   |           |   |      |    |
+                |           |   |           |   |      |    |
+                |           |   |           |   |      |    |
+                |         * |   |         * |   |    * |    |
+                |           |   |           |   |      |    |
+                |    [L]    |   |    [M]    |   |    /      |
+                |___________|   |___________|   |__/________|
+
+                You opend the right door and {random_encounter(rand_index)}
+                """)
+                time.sleep(0.3)
+                clearConsole()
+                print(f"""
+                _____________   _____________   _____________
+                |           |   |           |   |  |        |
+                |           |   |           |   |  |        |
+                |           |   |           |   | .|        |
+                |         * |   |         * |   |  |        |
+                |           |   |           |   |  |        |
+                |    [L]    |   |    [M]    |   | /         |
+                |___________|   |___________|   |/__________|
+
+                You opend the right door and {random_encounter(rand_index)}
+                """)
             right_open = True
-        elif choose_door == "q":
+        elif choose_door == "q":  # BACK TO MENU
             clearConsole()
             print("Returning to menu...")
         if left_open == True and middle_open == True and right_open == True:
-            clearConsole()
-            print("You have no doors left\nReturning to menu...")
-            break
+            quit = input(
+                "You have no doors left\nReturn to menu [Q]").casefold()
+            if quit == "q":
+                print("Returning to menu...")
+                break
 
 
 def inventory():
     burgir = Items("Heals you for __HP", "Burgir")
-    your_items = []
+    your_items = [burgir.name]
     try:
         item_one = your_items[0]
     except:
@@ -110,9 +241,9 @@ def inventory():
     except:
         item_five = ""
     print(f"""
-            ----------------INVENTORY----------------
+            ----------------INVENTORY-----------------
             |   {item_one}    |   {item_two}    |   {item_three}    |   {item_four}    |   {item_five}    |
-            -----------------------------------------
+            ------------------------------------------
     """)
     while True:
         inv = int(input("            Select your item 1-5 -> "))
@@ -120,37 +251,37 @@ def inventory():
         if inv == 1:
             clearConsole()
             print(f"""
-            ----------------INVENTORY---------------
+            ----------------INVENTORY----------------
             |  ->{item_one}<-  |   {item_two}    |   {item_three}    |   {item_four}    |   {item_five}    |
-            ----------------------------------------
+            -----------------------------------------
             """)
         elif inv == 2:
             clearConsole()
             print(f"""
-            ----------------INVENTORY---------------
+            ----------------INVENTORY----------------
             |   {item_one}    |  ->{item_two}<-  |   {item_three}    |   {item_four}    |   {item_five}    |
-            ----------------------------------------
+            -----------------------------------------
             """)
         elif inv == 3:
             clearConsole()
             print(f"""
-            ----------------INVENTORY----------------
+            ----------------INVENTORY-----------------
             |   {item_one}    |   {item_two}   |   ->{item_three}<-   |   {item_four}    |   {item_five}    |
-            -----------------------------------------
+            ------------------------------------------
             """)
         elif inv == 4:
             clearConsole()
             print(f"""
-            ----------------INVENTORY----------------
+            ----------------INVENTORY-----------------
             |   {item_one}    |   {item_two}   |   {item_three}    |   ->{item_four}<-   |   {item_five}    |
-            -----------------------------------------
+            ------------------------------------------
             """)
         elif inv == 5:
             clearConsole()
             print(f"""
-            ----------------INVENTORY----------------
+            ----------------INVENTORY-----------------
             |   {item_one}    |   {item_two}   |   {item_three}    |   {item_four}    |   ->{item_five}<-   |
-            -----------------------------------------
+            ------------------------------------------
             """)
 
 
